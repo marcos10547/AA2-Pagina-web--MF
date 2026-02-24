ef2ae56fd9258d219fcbcdc7c2b82885b8a2782a
marcos10547 <160898439+marcos10547@users.noreply.github.com>
2026-02-24 19:05:30 +0100

    cmabios

---
+++ frontend/src/vuetify-shims.d.ts
@@ -0,0 +1,14 @@
+declare module 'vuetify/styles' {
+    const styles: any;
+    export default styles;
+}
+
+declare module 'vuetify/components' {
+    const components: any;
+    export { components };
+}
+
+declare module 'vuetify/directives' {
+    const directives: any;
+    export { directives };
+}
