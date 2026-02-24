f0014a7e474079da5e0f58506f5159d0c395c5cd
marcos10547 <160898439+marcos10547@users.noreply.github.com>
2026-02-24 18:46:35 +0100

    instalación y configuración de vuetify con diseño base

---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,31 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+    theme: {
+        defaultTheme: 'light',
+        themes: {
+            light: {
+                colors: {
+                    primary: '#1867C0',
+                    secondary: '#5CBBFF',
+                },
+            },
+        },
+    },
+})
+
+export default vuetify
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="home">
-    <h1>Página Principal (Pública)</h1>
-    <p>Pronto configuraremos el diseño con Vuetify.</p>
-    <router-link to="/login">Ir al Login</router-link>
-  </div>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-card class="mt-5" elevation="2">
+          <v-card-title class="text-h4 primary--text">
+            <v-icon icon="mdi-coffee" class="mr-2"></v-icon>
+            Gestión de Inventario - Cafetería
+          </v-card-title>
+          
+          <v-card-text>
+            <p class="text-body-1">Bienvenido a la aplicación de gestión. Esta es la página principal pública.</p>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="primary" variant="elevated" to="/login">
+              <v-icon icon="mdi-login" class="mr-1"></v-icon>
+              Ir al Login
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
